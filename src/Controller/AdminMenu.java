d1f7091fa4ee6b6ced2d9a82814c26ec850e3f32
ayse <ayse_775@hotmail.com>
2019-05-15 18:22:12 +0200

    + admin menu and sign in added

--- src/Controller/AdminMenu.java
+++ src/Controller/AdminMenu.java
@@ -1,4 +1,63 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import sample.ChangeScene;
+
+import java.io.IOException;
+
 public class AdminMenu {
+
+    @FXML
+    private Button volunteersButton;
+
+    @FXML
+    private Button addEventsButton;
+
+    @FXML
+    private Button signOutButton;
+
+    @FXML
+    private Button viewEventsButton;
+
+    ChangeScene cs = new ChangeScene();
+
+    @FXML
+    void handleAddEvents(ActionEvent event) throws IOException {
+
+        cs.sceneHandler("../View/AddEvent.fxml", event);
+
+
+    }
+
+    @FXML
+    void handleViewEvents(ActionEvent event) throws  IOException{
+        cs.sceneHandler("../View/Events.fxml", event);
+
+
+
+    }
+
+    @FXML
+    void handleViewVolunteers(ActionEvent event) throws IOException {
+        cs.sceneHandler("../View/DefaultPage.fxml", event);
+
+    }
+
+
+
+
+    @FXML
+    void handleSignOutButton(ActionEvent event) throws  IOException {
+        cs.sceneHandler("../View/DefaultPage.fxml", event);
+
+    }
+
+
+
 }
